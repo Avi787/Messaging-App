170d67801e3d15c19134edc9f80632377d496faa
Avi787 <42003468+Avi787@users.noreply.github.com>
2019-02-16 22:10:00 +0200

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -22,8 +22,8 @@ ActiveRecord::Schema.define(version: 20190211014042) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
