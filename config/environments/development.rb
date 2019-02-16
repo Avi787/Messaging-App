7e5386f6e26a6aa754143f2d150e817020a87cc4
Avi Tasks <workavi78@gmail.com>
2019-02-16 21:24:00 +0200

    Final committed code

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,6 +40,8 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
+  # config.active_record.verbose_query_logs = true
+  
   config.assets.debug = true
 
   # Suppress logger output for asset requests.
@@ -47,8 +49,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # config.action_cable.url = "ws://localhost:3000/cable"
 end
